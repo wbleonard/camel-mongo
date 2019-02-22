44ff4dcd57667ab0851ed73fd06417dbb367030a
Blaine Mincey <blaine.mincey@gmail.com>
2019-02-22 11:16:12 -0500

    Added RouteBuilder class.

---
+++ src/main/java/com/mongodb/camelstarter/CamelStarterRoute.java
@@ -0,0 +1,22 @@
+package com.mongodb.camelstarter;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CamelStarterRoute extends RouteBuilder {
+
+    @Autowired
+    private PrintBeans printBeans;
+
+    @Override
+    public void configure() {
+        System.out.println("Hello, World!");
+        printBeans.printBeans();
+
+        //from("direct:findById")
+        //.to("mongodb3:mongoBean?database=flights&collection=tickets&operation=findById")
+        //.to("mock:resultFindById");
+    }
+}
--- src/main/java/com/mongodb/camelstarter/PrintBeans.java
+++ src/main/java/com/mongodb/camelstarter/PrintBeans.java
@@ -1,15 +1,19 @@
 package com.mongodb.camelstarter;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
+@Component
 public class PrintBeans {
     @Autowired
     ApplicationContext applicationContext;
 
     public void printBeans() {
+        System.out.println("===== My Beans =====");
         System.out.println(Arrays.asList(applicationContext.getBeanDefinitionNames()));
+        System.out.println("====== Completed Spilling the Beans =====");
     }
 }
